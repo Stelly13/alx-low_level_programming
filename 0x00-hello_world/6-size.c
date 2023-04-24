67c541fb46637ac17c6f3797d64f5489929eac87
Blissy Ikazzy <blissyikazzy@gmail.com>
2023-04-24 03:36:53 -0700

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - A C program that prints the size of various types on the computer
+* Return: Always 0
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)", sizeo f(char);
+	printf("Size of an int: 4 byte(s)", sizeo f(int);
+	printf("Size of a long int: 4 byte(s)", sizeo f(long int);
+	printf("Size of a long long int: 8 byte(s)", sizeo f(long long int);
+	printf("Size of a float: 4 byte(s)", sizeo f(float);
+	return (0);
+	}
