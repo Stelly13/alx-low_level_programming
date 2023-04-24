401dc79e83d3f67faf2bf003bdaf577abc685237
Blissy Ikazzy <blissyikazzy@gmail.com>
2023-04-24 15:07:52 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return : always 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('/n');
+	return (0);
