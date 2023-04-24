f65f78f182db87736429fe40b402c412f400c203
Blissy Ikazzy <blissyikazzy@gmail.com>
2023-04-24 04:36:40 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisto.h>
+/**
+ * main - A C program that prints new line to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
