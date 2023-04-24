40ea28ff9a241a4026165dfc51b664db0b9b935f
Blissy Ikazzy <blissyikazzy@gmail.com>
2023-04-24 14:54:38 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints the alphabet in lowercase.
+ * Return: always 0
+ */
+int main(void)
+{
+	char lc;
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc);
+	}
+		putchar('/n');
+		return (0);
+}
+
+
